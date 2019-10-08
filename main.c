9c209c11f66bd7a96abb3cd385766accb72831d8
soomin0409 <55428420+soomin0409@users.noreply.github.com>
2019-10-08 15:09:00 +0900

    04

--- main.c
+++ main.c
@@ -4,18 +4,12 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int input;
-	printf("input : ");
-	scanf("%d",&input);
-	if(input>0)
+	int i=0,meter;
+	while(i<3)
 	{
-		printf("positive");
+		meter= i* 1609;
+		printf("%d mile is %d meter.\n",i,meter);
+		i++;
 	}
-	else if(input<0)
-	{
-		printf("negative");
-	}
-	else
-	printf("0");
 	return 0;
 }
